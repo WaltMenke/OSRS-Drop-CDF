15d22051f6357b9814ff04b2c2bb41afdc5c4443
Walter Menke <wmenke597@gmail.com>
2024-04-18 19:56:54 -0400

    Added some rarity colors, threshold line, and complementary probability chances.

--- OSRS_CDF_App.py
+++ OSRS_CDF_App.py
@@ -1,6 +1,7 @@
 from osrsbox import monsters_api
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 
 app = Dash(__name__)
@@ -8,12 +9,11 @@ all_monsters = monsters_api.load()
 
 MAX_KILLS = 5000
 RARITY_SPECS = {
-    "Always": {"color": "#AFEEE", "range": (1 / 1, 1 / 1)},
+    "Always": {"color": "#AFEEEE", "range": (1 / 1, 1 / 1)},
     "Common": {"color": "#56E156", "range": (1 / 2, 1 / 25)},
     "Uncommon": {"color": "#FFED4C", "range": (1 / 26, 1 / 99)},
     "Rare": {"color": "#FF863C", "range": (1 / 100, 1 / 999)},
-    "Very rare": {"color": "#FF6262", "range": (1 / 1000,)},
-    "Varies": {"color": "#FFA3FF", "range": None},
+    "Very rare": {"color": "#FF6262", "range": (1 / 1000, 0)},
 }
 
 
@@ -22,7 +22,7 @@ def get_all_monsters(database):
     monster_options = [
         (monster.name, monster.combat_level)
         for monster in all_monsters
-        if monster.name is not None
+        if monster.name is not None and monster.drops
     ]
     monster_options = sorted(set(monster_options))
     monster_options = [
@@ -32,7 +32,6 @@ def get_all_monsters(database):
     return monster_options
 
 
-# Set up initial layout with 2 dropdowns and 1 graph
 app.layout = html.Div(
     [
         html.H1(
@@ -117,7 +116,9 @@ app.layout = html.Div(
                 "justifyContent": "left",
             },
         ),
-        dcc.Graph(id="graph-cdf"),
+        dcc.Graph(
+            id="graph-cdf",
+        ),
     ],
     className="justify-content-center",
 )
@@ -138,7 +139,7 @@ def update_monster_options(monster):
         return [{"label": "Enter monster name first", "value": "None"}]
 
 
-def drop_cdf(rarity, kills):
+def calculate_cdf(rarity, kills):
     """Based on the binomial distribution, probability of observing success at least once in kills amount of independent Bernoulli trials"""
     cdf = 1 - (1 - rarity) ** kills
     return cdf
@@ -178,7 +179,7 @@ def search_monster(monster_in):
         Input("drop-test", "value"),
     ],
 )
-def calculate_cdf(selected_enemy, selected_drop, num_kills, drop_chance):
+def plot_cdf(selected_enemy, selected_drop, num_kills, drop_chance):
     if selected_enemy and selected_drop and num_kills != 0:
         find_monster = search_monster(selected_enemy)
         if find_monster:
@@ -189,30 +190,64 @@ def calculate_cdf(selected_enemy, selected_drop, num_kills, drop_chance):
         data = {
             "Number of Kill Attempts (n)": range(1, num_kills + 1),
             "Cumulative Probability": [
-                drop_cdf(rarity, i) for i in range(1, num_kills + 1)
+                calculate_cdf(rarity, i) for i in range(1, num_kills + 1)
             ],
         }
         df = pd.DataFrame(data)
         filt_df = df[df["Cumulative Probability"] > int(drop_chance) / 100]
         if not filt_df.empty:
             kill_threshold = filt_df.iloc[0, 0]
-            cdf_output = f"{drop_chance}% chance of receiving at least one {selected_drop} (1/{int(1/rarity)}) from {selected_enemy} at {kill_threshold} kills!"
-        else:
-            complement_probability = filt_df.iloc[-1, 0]
-            cdf_output = f"Yikes! Less than {complement_probability}% chance to receive at least one {selected_drop} (1/{int(1/rarity)}) from {selected_enemy} at {num_kills} kills."
+            cdf_output = f"{drop_chance}% chance of receiving at least one {selected_drop} from {selected_enemy} at {kill_threshold} kills!"
+            hit_rate = 1
+        elif int(round(df.iloc[-1, -1] * 100, 0)) != int(drop_chance):
+            most_likely_probability = df.iloc[-1, -1]
+            cdf_output = f"Uh-oh! You only have a {int(round(most_likely_probability*100, 0))}% chance to receive at least one {selected_drop} from {selected_enemy} at {num_kills} kills."
+            hit_rate = 0
 
+        rarity_color, _ = get_rarity_color(rarity)
+        rarity_color = [rarity_color]
         fig = px.line(
             df,
             x="Number of Kill Attempts (n)",
             y="Cumulative Probability",
             title=cdf_output,
+            color_discrete_sequence=rarity_color,
         )
         fig.update_layout(title_x=0.5)
+        fig.update_traces(line=dict(width=4))
+        x = fig.data[0]["x"]
+        y = fig.data[0]["y"]
+        fig.add_trace(
+            go.Scatter(
+                x=x,
+                y=y,
+                fill="tozeroy",
+                fillcolor=rarity_color[0],
+                mode="none",
+                showlegend=False,
+            )
+        )
+        if hit_rate:
+            fig.add_shape(
+                type="line",
+                x0=min(x),
+                x1=max(x),
+                y0=drop_chance / 100,
+                y1=drop_chance / 100,
+                line=dict(color="black", width=2, dash="solid"),
+            )
         return fig
     else:
         return {
             "layout": {"title": "Select an enemy, item, and kill count to plot the CDF"}
         }
+
+
+def get_rarity_color(rarity):
+    for category, specs in RARITY_SPECS.items():
+        if specs["range"][0] >= rarity >= (specs["range"][1]):
+            return specs["color"], category
+    return "black", "Unknown"
 
 
 @app.callback(
@@ -223,29 +258,25 @@ def calculate_cdf(selected_enemy, selected_drop, num_kills, drop_chance):
         Input("kill-count", "value"),
     ],
 )
-def update_info(selected_enemy, selected_drop, num_kills):
+def instance_info(selected_enemy, selected_drop, num_kills):
     if selected_enemy and selected_drop and num_kills != 0:
         find_monster = search_monster(selected_enemy)
         if find_monster:
             drops = get_drops(find_monster)
 
         rarity = get_rarity(drops, selected_drop)
-        for rarity_level, specs in RARITY_SPECS.items():
-            if specs["range"] and (int(1 / rarity)) <= specs["range"][0]:
-                rarity_color = specs["color"]
-                break
-            else:
-                rarity_color = "black"
+        rarity_color, rarity_category = get_rarity_color(rarity)
         rarity = f"1/{int(1/rarity)}"
         return [
-            "ENEMY: ",
-            selected_enemy,
             html.Br(),
-            "DROP: ",
-            selected_drop,
             html.Br(),
-            html.Span("RARITY: ", style={"color": rarity_color}),
-            html.Span(str(rarity), style={"color": rarity_color}),
+            html.Br(),
+            html.Br(),
+            html.Span("RARITY: ", style={"background-color": rarity_color}),
+            html.Span(
+                f"{rarity} ({rarity_category})",
+                style={"background-color": rarity_color},
+            ),
             html.Br(),
             "KILL COUNT: ",
             str(num_kills),
